a73b91fb083a364a741bb1cb009120af9af86074
Osman Corluk <corluk@hotmail.com>
2021-05-10 02:10:48 +0300

    d

--- bin/create.js
+++ bin/create.js
@@ -5,7 +5,7 @@ const packageJSON  = require("../resources/package.json")
 const exec = require("child_process").execSync
 const dependencies =  Object.keys(packageJSON.dependencies) 
 const devDependencies = Object.keys(packageJSON.devDependencies)
-if(proccess.argv.length < 3){
+if(process.argv.length < 3){
     throw new Error("you must set folder name to install ")
 }
 console.log(process.argv)
