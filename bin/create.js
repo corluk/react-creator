2455bcebb2e9e72678e7da9491a9259f46b568b3
Osman Corluk <corluk@hotmail.com>
2021-05-10 02:07:55 +0300

    d

--- bin/create.js
+++ bin/create.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node 
 
 
-const packageJSON  = require("../../package.json") 
+const packageJSON  = require("../resources/package.json") 
 const exec = require("child_process").execSync
 const dependencies =  Object.keys(packageJSON.dependencies) 
 const devDependencies = Object.keys(packageJSON.devDependencies)
